f6b94769ceebf121c542bf0fdcd06f9711f9ee78
1nkret <dima.dls.ua@gmail.com>
2024-07-04 13:43:54 +0300

    5 posts on 1 page. Now we can switch page

--- apps/blog/views/__init__.py
+++ apps/blog/views/__init__.py
@@ -1,4 +1,5 @@
 from .index import index
+from .index import index_page
 from .new_post import new_post
 from .edit_post import edit_post
 from .delete_post import delete_post
--- apps/blog/views/index.py
+++ apps/blog/views/index.py
@@ -1,19 +1,34 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from ..models import Post
 
 
 def index(request):
     """
-    Blog index page
+    Blog main index page
     :param request: request object
     :return: render index page
     """
-    last_posts = Post.objects.order_by('-date')[:5]
-    context = {"title": "Blog", "posts": last_posts}
+    posts = Post.objects.order_by('-date')[:5]
+    context = {"title": "Blog", "posts": posts}
 
     return render(request=request,
                   template_name='blog/index.html',
                   context=context)
 
 
+def index_page(request, page_id):
+    """
+    Blog index page (example: /blog/page_id)
+    :param request: request object
+    :param page_id: page id
+    :return: render index page
+    """
+    paginator = Paginator(Post.objects.order_by("-date"), 5)
+    posts = paginator.page(page_id)
+    context = {"title": "Blog", "posts": posts}
+
+    return render(request=request,
+                  template_name='blog/index.html',
+                  context=context)
