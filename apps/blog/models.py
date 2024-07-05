122696f66495ab67cb5e5be7b05c522035a7869e
1nkret <dima.dls.ua@gmail.com>
2024-07-05 13:37:46 +0300

    new style on code, fix title chars (2000->60)

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -5,8 +5,8 @@ class Post(models.Model):
     """
     Model Form
     """
-    title = models.CharField(max_length=255)
-    description = models.TextField()
+    title = models.CharField(max_length=60)
+    description = models.CharField(max_length=4000)
     date = models.DateTimeField(auto_now=True)
 
 
