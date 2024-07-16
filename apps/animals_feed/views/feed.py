e648cdc11d7c960a905f8dd604cf54d30b58a9f4
1nkret <dima.dls.ua@gmail.com>
2024-07-16 14:39:45 +0300

    sorting feed

--- apps/animals_feed/views/feed.py
+++ apps/animals_feed/views/feed.py
@@ -1,12 +1,19 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Max, F, Case, When
 from django.shortcuts import render
 
 from apps.animals_crm import models
 
 
 def feed(request):
-    paginator = Paginator(models.AnimalCard.objects.order_by("-created_at"), 3)
+    animal_cards = models.AnimalCard.objects.annotate(
+        last_comment_date=Max("comments__created_at"),
+        last_update_date=Case(
+            When(last_comment_date__isnull=True, then=F("modified_at")), default=F("last_comment_date")
+        ),
+    ).order_by("-last_update_date", "-modified_at")
 
+    paginator = Paginator(animal_cards, 3)
     page_obj = request.GET.get("page")
 
     try:
