c5c1fa7bd02471777c35725ea95870da85419bc5
DaTrEvTeR <rozhkovm176@gmail.com>
2024-07-17 14:31:39 +0300

    feat: remove card view from crm

--- apps/animals_crm/urls.py
+++ apps/animals_crm/urls.py
@@ -8,7 +8,6 @@ urlpatterns: list[path] = [
     path("", views.crm_index, name="crm"),
     path("new-card", views.new_card, name="new_card"),
     # work with 1 card
-    path("<int:card_id>", views.animal_card, name="animal_card"),
     path("edit/<int:card_id>", views.edit_card, name="edit_card"),
     path("delete/<int:card_id>", views.delete_card, name="delete_card"),
 ]
