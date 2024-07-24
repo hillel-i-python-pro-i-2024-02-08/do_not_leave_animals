96e826014b5651dcb09a6a95454f6fc70c0556a0
DaTrEvTeR <rozhkovm176@gmail.com>
2024-07-24 22:39:53 +0300

    ✨ Set celery and rabbitmq settings and move post saving in db on celery task

--- apps/blog/views/new_post.py
+++ apps/blog/views/new_post.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect, HttpResponseForbidden
 from django.shortcuts import render
 
 from apps.blog.forms import PostForm
+from ..tasks import save_post
 
 
 def new_post(request):
@@ -13,10 +14,8 @@ def new_post(request):
     if request.user.is_staff:
         context = {"title": "New Post", "form": PostForm}
         if request.method == "POST":
-            form = PostForm(request.POST, request.FILES)
-            if form.is_valid():
-                form.save()
-                return HttpResponseRedirect("/blog/")
+            save_post(request.POST, request.FILES)
+            return HttpResponseRedirect("/blog/")
         elif request.method == "GET":
             return render(request=request, template_name="blog/new_post.html", context=context)
     else:
